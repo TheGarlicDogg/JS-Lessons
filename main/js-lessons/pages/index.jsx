2b260a4c1dff4dff07b38367a828347cecf597af
GarlicDogg <garlicdogg@mail.ru>
2022-11-13 01:22:11 +0300

    Finished 3rd HW

---
+++ main/js-lessons/pages/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text } from  "../components/Text/Text";
+
+export default function IndexPage() {
+  return (
+    <main>
+      <Text>Тут я буду писать текст, который окажется в тэге p и будет стилизован</Text>
+    </main>
+  )
+}
