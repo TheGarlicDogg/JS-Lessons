b9d02130858a9e31ef06be5c151f00a985ccf141
GarlicDogg <garlicdogg@mail.ru>
2022-11-20 03:02:49 +0300

    hw4

--- main/js-lessons/pages/index.jsx
+++ main/js-lessons/pages/index.jsx
@@ -1,10 +1,32 @@
-import React from "react";
-import { Text } from  "../components/Text/Text";
+import React, { useEffect, useState } from "react";
+import { Button } from "../components/Button/Button";
+import {List} from "../components/List/List"
 
 export default function IndexPage() {
+  let [content, setContent] = useState(null);
+  let [value1, setValue1] = useState("");
+  let [value2, setValue2] = useState("");
+  let [page, setPage] = useState(0);
+  
+  function handleClick(e) {
+    e.preventDefault();
+    setContent(lastState => [{url: value1}, ...(lastState || []), ]);
+  }
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/photos")
+      .then(res => res.json())
+      .then(obj => setContent(obj));
+  },[])
   return (
-    <main>
-      <Text>Тут я буду писать текст, который окажется в тэге p и будет стилизован</Text>
-    </main>
+    <div>
+      <form onSubmit={handleClick}>
+        <input type="text" value={value1} onChange={e => setValue1(e.target.value)}/>&nbsp;
+        <Button type="submit">Добавить изображение</Button><br/>
+        <Button type="button" onClick={() => setPage(p => p+1)}>
+          Страница {page+1}
+        </Button>
+      </form>
+      {content && <List content={content.slice(page*10, (page+1)*10)} />}
+    </div>
   )
 }
