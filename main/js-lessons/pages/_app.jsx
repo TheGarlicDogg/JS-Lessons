ed04b68a3d6dbe4f2faff02499cb3868f0b304d9
GarlicDogg <garlicdogg@mail.ru>
2022-12-30 00:35:01 +0300

    finished 8 lesson

--- main/js-lessons/pages/_app.jsx
+++ main/js-lessons/pages/_app.jsx
@@ -1,5 +1,5 @@
 import "../styles/style.css"
-
+import "highlight.js/styles/dark.css"
 export default function MyApp({ Component, pageProps}) {
     return <Component {...pageProps} />
 }
--- main/js-lessons/pages/sign-in/index.jsx
+++ main/js-lessons/pages/sign-in/index.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import { Input } from '../../components/UI/Input/Input'
+import { Button } from '../../components/UI/Button/Button'
+import Link from 'next/link'
+import { useState } from 'react'
+
+import css from "./sign-in.module.css";
+
+
 
 const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  function FormHandler (e) {
+    e.preventDefault();
+    fetch("https://norma.nomoreparties.space/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json", 
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    }).then(res=>{
+      if (res.ok) return e.json();
+      throw Error();
+    }, console.log)
+    .then(console.log)
+  }
   return (
-    <div>SignIn</div>
+    <form className={css.form} onSubmit={FormHandler}>
+      <fieldset className={css.form__inputs}>
+        <legend>Вход в аккаунт</legend>
+          <Input onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Почта" value={email}required>
+            Почта
+          </Input>
+          <Input onChange={(e) => setPassword  (e.target.value)} type="password" placeholder="Пароль" value={password}required>
+            Пароль
+          </Input>
+          </fieldset>
+          <Link href="/register">Зарегистрироваться</Link>
+          <Button type="submit">Войти</Button>
+    </form>
   )
 }
 
